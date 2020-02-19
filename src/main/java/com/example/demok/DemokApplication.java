acc67e3a145536d9f152167f936e5e7548a52350
Facucode <34733879+Facucode@users.noreply.github.com>
2020-02-19 17:17:51 -0300

    first commit

---
+++ src/main/java/com/example/demok/DemokApplication.java
@@ -0,0 +1,13 @@
+package com.example.demok;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemokApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemokApplication.class, args);
+	}
+
+}
